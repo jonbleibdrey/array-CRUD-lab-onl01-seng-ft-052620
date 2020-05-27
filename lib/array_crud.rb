96bc15fb1c3ec8001a3819a952d11639835b8bd9
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-27 18:05:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,13 +19,13 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array=["happy","sad","hungry","full"]
+  array=[,"happy","sad","hungry","full","arrays!"]
   array.pop
 
 end
 
 def remove_element_from_start_of_array(array)
-  array=["happy","sad","hungry","full","arrays!"]
+  array=["wow","happy","sad","hungry","full"]
   array.shift
 
 end
